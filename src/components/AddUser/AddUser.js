b4896104369cd56cc204b9e6b1eb84fc776a28f4
salman602 <salmanrahman602@gmail.com>
2021-10-26 10:33:13 +0600

    first commit

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const AddUser = () => {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const handleAddUser = (e) => {
+        e.preventDefault();
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+        const newUser = { name, email };
+
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    alert('Successfully added the user.')
+                    e.target.reset()
+                }
+            })
+
+    }
     return (
         <div>
             <h2>This is Add User</h2>
+            <form onSubmit={handleAddUser}>
+                <input type="text" placeholder="Name" ref={nameRef} /> <br />
+                <input type="email" name="" id="" placeholder="Email" ref={emailRef} /> <br />
+                <input type="submit" value="Add" />
+            </form>
         </div>
     );
 };
--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 
 const UpdateUser = () => {
+    const {id} = useParams();
+    const [user, setUser] = useState({});
+
+    useEffect(()=>{
+        const url = `http://localhost:5000/users/${id}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setUser(data))
+    },[])
+
+
     return (
         <div>
-            <h2>This is Update User</h2>
+            <h2>Update User Name: {user.name}</h2>
+            <p>{id}</p>
+            <p>Email: {user.email}</p>
         </div>
     );
 };
